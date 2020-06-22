9078e46a453c4c2b02dbe2283c95d54aabf6d1d3
ELINANI <elinanisoufiane@gmail.com>
2020-06-22 05:55:09 +0200

    JSF and MySql CRUD

---
+++ src/ma/Users/config/applicationConfiguration.java
@@ -0,0 +1,10 @@
+package ma.Users.config;
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig( version = FacesConfig.Version.JSF_2_3 ) 
+public class applicationConfiguration {
+	
+
+}
